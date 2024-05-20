7a59ad674055c7efdb946031f79f80b826d463a8
Aung-IO <apkdust@gmail.com>
2024-05-20 22:33:15 +0630

    credential provider

--- auth.ts
+++ auth.ts
@@ -1,6 +1,30 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    Google,
+    Credentials({
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "example@gmail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials: any) {
+        if (
+          credentials.email === "apkdust@gmail.com" &&
+          credentials.password === "apkdust"
+        )
+          return {
+            name: "apwiz",
+          };
+        else return null;
+      },
+    }),
+  ],
 });
